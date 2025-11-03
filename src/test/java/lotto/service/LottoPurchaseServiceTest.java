b2804035b0fddca5cf0c4cee59fc380996310863
sihyun10 <hyhy200170@gmail.com>
2025-11-03 12:22:57 +0900

    test(LottoPurchaseService): 입력 금액에 따라 올바른 개수의 로또가 생성되는지 테스트
    입력 금액에 따라 올바른 개수의 로또가 생성되는지 테스트해보았다.
    테스트하기 위해 FakePurchaseCalculator, FakeNumberGenerator를 사용하였다.
    구매 결과의 첫 번째 로또 번호가 예상값과 일치하는지 확인하였다.

---
+++ src/test/java/lotto/service/LottoPurchaseServiceTest.java
@@ -0,0 +1,46 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.service.calculator.LottoPurchaseCalculator;
+import lotto.util.LottoNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoPurchaseServiceTest {
+
+    private LottoPurchaseService purchaseService;
+
+    @BeforeEach
+    void setUp() {
+        FakePurchaseCalculator fakePurchaseCalculator = new FakePurchaseCalculator();
+        FakeNumberGenerator fakeNumberGenerator = new FakeNumberGenerator();
+        purchaseService = new LottoPurchaseService(fakePurchaseCalculator, fakeNumberGenerator);
+    }
+
+    @Test
+    @DisplayName("입력 금액에 따라 올바른 개수의 로또가 생성된다.")
+    void 로또_구입() {
+        List<Lotto> result = purchaseService.purchase("3000");
+
+        assertThat(result).hasSize(3);
+        assertThat(result.getFirst().numbers()).containsExactly(17, 19, 29, 34, 39, 40);
+    }
+
+    static class FakePurchaseCalculator extends LottoPurchaseCalculator {
+        @Override
+        public int calculateCount(String inputAmount) {
+            return Integer.parseInt(inputAmount) / 1000;
+        }
+    }
+
+    static class FakeNumberGenerator extends LottoNumberGenerator {
+        @Override
+        public List<Integer> generate() {
+            return List.of(17, 19, 29, 34, 39, 40);
+        }
+    }
+}
